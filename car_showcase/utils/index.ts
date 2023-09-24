6c4799c17ccd96cac4bdb12655fd1ce5b4676457
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-09-24 12:24:39 +0530

    installed : dotenv

--- car_showcase/utils/index.ts
+++ car_showcase/utils/index.ts
@@ -1,10 +1,11 @@
 import { CarProps, FiltersProps } from "@/types";
+require("dotenv").config();
 
 export async function fetchCars(filters: FiltersProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
   const headers = {
-    "X-RapidAPI-Key": "fd4df3aaf5msh2fedeb1f1a2340fp1d4a69jsndcc77bdba5d1",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.X_Rapid_API_KEY,
+    "X-RapidAPI-Host": process.env.X_Rapid_API_HOST,
   };
   const response = await fetch(
     `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
