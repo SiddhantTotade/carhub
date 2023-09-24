4b74a82223d7d441a66ebcaa0d4aab8a92705ddf
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-09-24 17:14:42 +0530

    modified : SearchBar and SearchManufacturer

--- carhub/components/SearchManufacturer.tsx
+++ carhub/components/SearchManufacturer.tsx
@@ -50,36 +50,45 @@ const SearchManufacturer = ({
             afterLeave={() => setQuery("")}
           >
             <Combobox.Options>
-              {filteredManufacturers.map((item) => (
+              {filteredManufacturers.length === 0 && query !== "" ? (
                 <Combobox.Option
-                  key={item}
-                  className={({ active }) =>
-                    `relative search-manufacturer__option ${
-                      active ? "bg-primary-blue text-white" : "text-gray-900"
-                    }`
-                  }
-                  value={item}
+                  value={query}
+                  className="search-manufacturer__option"
                 >
-                  {({ selected, active }) => (
-                    <>
-                      <span
-                        className={`block truncate ${
-                          selected ? "font-medium" : "font-normal"
-                        }`}
-                      >
-                        {item}
-                      </span>
-                      {selected ? (
-                        <span
-                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                            active ? "text-white" : "text-teal-600"
-                          }`}
-                        ></span>
-                      ) : null}
-                    </>
-                  )}
+                  Create "{query}"
                 </Combobox.Option>
-              ))}
+              ) : (
+                filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? "bg-primary-blue text-white" : "text-gray-900"
+                      }`
+                    }
+                    value={item}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? "text-white" : "text-teal-600"
+                            }`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
             </Combobox.Options>
           </Transition>
         </div>
