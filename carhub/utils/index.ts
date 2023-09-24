9f6d06e43fb6c71ac043bd627daea4936f74ac18
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-09-24 13:40:26 +0530

    modified : index.ts

--- carhub/utils/index.ts
+++ carhub/utils/index.ts
@@ -7,8 +7,7 @@ export async function fetchCars(filters: FiltersProps) {
     "X-RapidAPI-Host": process.env.NEXT_PUBLIC_X_Rapid_API_HOST,
   };
   const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-    { headers: headers }
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,{ headers: headers }
   );
 
   const result = await response.json();
