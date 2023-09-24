142b607695452e95e9dcccd93d22bc2c76d37398
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-09-24 13:20:45 +0530

    modified : .env file

--- car_showcase/utils/index.ts
+++ car_showcase/utils/index.ts
@@ -1,11 +1,10 @@
 import { CarProps, FiltersProps } from "@/types";
-require("dotenv").config();
 
 export async function fetchCars(filters: FiltersProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
   const headers = {
-    "X-RapidAPI-Key": process.env.X_Rapid_API_KEY,
-    "X-RapidAPI-Host": process.env.X_Rapid_API_HOST,
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_Rapid_API_KEY,
+    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_X_Rapid_API_HOST,
   };
   const response = await fetch(
     `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
